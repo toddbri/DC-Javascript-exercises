f4930b9d4df30eff498d57e8dd7802690e8fdacc
Todd Briley <todd.briley@outlook.com>
2017-03-01 11:37:12 -0500

    adding additional exercises

---
+++ printNumbers.js
@@ -0,0 +1,14 @@
+function printNumbers(x,y){
+  for (var i=x;i<=y;i++){
+    console.log(i);
+  }
+}
+console.log("using for loop");
+printNumbers(1,10);
+
+console.log("using while loop");
+i =1;
+while (i<=10){
+  console.log(i);
+  i++;
+}
